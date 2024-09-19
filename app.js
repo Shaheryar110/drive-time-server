589de9e451d4e841e5542d6c204197f016c9ca91
Shaheryar110 <mshaheryar35@gmail.com>
2024-09-20 00:31:09 +0500

    ush

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const app = express();
 const stripe = require("stripe")(
   "sk_test_51P2FrVRxtMIfYHGVHsANuSOsWDyRutmSBkuEy215Q6G6htFXR9WQlmgPfejhYophKhyYZitF085nyJzO0DgK8XMa00Xc3jYIpv"
 );
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "cpanel14.olivelogo.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "hugheyhartman@gmail.com", // Replace with your email
+    pass: "kqfb zaqm ipeg jlbu", // Replace with your email password
+  },
+});
 
 const port = 5001;
 app.use(express.json());
@@ -36,6 +47,24 @@ app.post("/drive-time/create-payment-intent", async (req, res) => {
     res.status(400).send({
       error: error.message,
     });
+  }
+});
+app.post("/send-email", async (req, res) => {
+  const { email, name, phone, message, selectedDay, selectedTime } = req.body;
+
+  const mailOptions = {
+    from: email, // User's email as the sender
+    to: "hugheyhartman@gmail.com", // Replace with admin's email
+    subject: "Your Appointment Has Been Schedeule ",
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}\nSchedule Day and Time: ${selectedDay} - ${selectedTime}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Failed to send email");
   }
 });
 
